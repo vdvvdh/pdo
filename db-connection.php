d85173e26320ad2bf3e44d59a0bb808140081231
vdvvdh <anxin.xie@student.rocva.nl>
2025-05-20 12:21:38 +0200

    registratie

--- db-connection.php
+++ db-connection.php
@@ -1,13 +1,24 @@
 <?php
-$host = 'localhost:3306';
-$db   = 'winkel2';
-$user = 'root';
-$pass = '';
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-} catch (PDOException $e) {
-    echo "error:" . $e-> getMessage();
+Class DB{
+    public $pdo;
+
+    public function __construct($db = "winkel2", $host="localhost", $user="root", $pass=""){
+        try {
+            $this->pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+            //set the PDO error mode to exception
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "Connection failed: ". $e->getMessage();
+        }
+    }
+
+    public function aanmelden($naam, $email, $password){
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $stmt = $this->pdo->prepare("INSERT into user (naam,email,password) VALUES (:naam,:email,:password)");
+        $stmt->execute(["naam"=>$naam, "email"=>$email, "password"=>$hash]);
+    }
+
 }
 ?>
 
---
+++ user/register-user.php
@@ -0,0 +1,32 @@
+<?php
+
+require "../db-connection.php";
+
+if (isset($_POST['submit'])) {
+    try {
+        $pdo->aanmelden($_POST['naam'], $_POST['email'], $_POST['password']);
+        echo "account aangemaakt";
+    } catch (PDOException $e) {
+        echo $e;
+    }
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="#">
+        <input type="name" name="naam" placeholder="username" required><br>
+        <input type="email" name="email" placeholder="email" required><br>
+        <input type="password" name="wachtwoord" placeholder="password" required><br>
+        <button type="submit" name="knop">Submit</button>
+    </form>
+</body>
+</html>
